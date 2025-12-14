425d3d74f7f0f81e111b31f6b2cbdd81e279ec1e
Nazeha mosharaf <nazehamosharaf@gmail.com>
2025-12-14 12:28:43 +0600

    Refactor Huffman coding to read text and calculate frequencies
    Refactored Huffman coding implementation to read text input directly and calculate frequencies dynamically. Updated code structure for generating Huffman codes and displaying results.

--- Huffman Coding (variable length).cpp
+++ Huffman Coding (variable length).cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 using namespace std;
-struct Node {
+
+struct Node 
+{
     char ch;
     int freq;
     Node *left, *right;
@@ -14,47 +16,81 @@ struct Node {
         left = right = nullptr;
     }
 };
+
 struct Compare {
     bool operator()(Node* a, Node* b) {
         return a->freq > b->freq;
     }
 };
-void printCodes(Node* root, string code) {
+
+void generateCodes(Node* root, string code, unordered_map<char, string> &huffmanCode) 
+{
     if (!root) return;
-    if (root->ch != '$') {
-        cout << root->ch << " : " << code << endl;
+
+    if (!root->left && !root->right) {
+        huffmanCode[root->ch] = code;
     }
-    printCodes(root->left, code + "0");
-    printCodes(root->right, code + "1");
+    generateCodes(root->left,  code + "0", huffmanCode);
+    generateCodes(root->right, code + "1", huffmanCode);
 }
 
-int main() {
-    int n;
-    cout << "Enter number of characters: ";
-    cin >> n;
-    vector<char> chars(n);
-    vector<int> freq(n);
-    cout << "Enter characters: ";
-    for (int i = 0; i < n; i++) cin >> chars[i];
-    cout << "Enter frequencies: ";
-    for (int i = 0; i < n; i++) cin >> freq[i];
-    priority_queue<Node*, vector<Node*>, Compare> pq;
-    for (int i = 0; i < n; i++) {
-        pq.push(new Node(chars[i], freq[i]));
+int main() 
+{
+    string text;
+
+    cout << "Enter text: ";
+    getline(cin, text);
+
+    unordered_map<char, int> freq;
+
+    for (char c : text) {
+        freq[c]++;
     }
-    while (pq.size() > 1) {
-        Node* left = pq.top(); pq.pop();
-        Node* right = pq.top(); pq.pop();
-        Node* newNode = new Node('$', left->freq + right->freq);
+
+    priority_queue<Node*, vector<Node*>, Compare> pq;
+
+    for (auto pair : freq) {
+        pq.push(new Node(pair.first, pair.second));
+    }
+
+    while (pq.size() > 1) 
+    {
+        Node *left = pq.top(); pq.pop();
+        Node *right = pq.top(); pq.pop();
+
+        Node *newNode = new Node('\0', left->freq + right->freq);
         newNode->left = left;
         newNode->right = right;
+
         pq.push(newNode);
     }
 
     Node* root = pq.top();
 
-    cout << "\nGenerated Huffman Codes:\n";
-    printCodes(root, "");
+    unordered_map<char, string> huffmanCode;
+    generateCodes(root, "", huffmanCode);
+
+    cout << "\nCharacter | Frequency | Huffman Code | Calculation\n";
+    cout << "-------------------------------------------------------\n";
+
+    int total_bits = 0;
+
+    for (auto pair : freq) {
+        char c = pair.first;
+        int f = pair.second;
+        string code = huffmanCode[c];
+
+        int bitsUsed = f * code.length();
+        total_bits += bitsUsed;
+
+        cout << "   " << c
+             << "      |     " << f
+             << "     |    " << code
+             << "        | " << f << " X " << code.length()
+             << " = " << bitsUsed << " bits\n";
+    }
+
+    cout << "\nTotal compressed size: " << total_bits << " bits\n";
 
     return 0;
 }
